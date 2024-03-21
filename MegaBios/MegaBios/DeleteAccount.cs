03ef6177ae7cc67fc3bc5ae9fdb139059d04ba7a
Maurice <mauriceboendermaker@gmail.com>
2024-03-21 12:37:13 +0100

    Moved DeleteAccount functions to its own file

---
+++ MegaBios/MegaBios/DeleteAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace MegaBios
+{
+    public class DeleteAccount
+    {
+        public static void RemoveAccount(List<TestAccount> jsonData, TestAccount account)
+        {
+            for (int i = 0; i < jsonData.Count; i++)
+            {
+                if (jsonData[i].Email == account.Email && jsonData[i].Wachtwoord == account.Wachtwoord)
+                {
+                    jsonData.RemoveAt(i);
+                    break;
+                }
+            }
+            JsonFunctions.WriteToJson("../../../customers.json", jsonData);
+        }
+    }
+}
